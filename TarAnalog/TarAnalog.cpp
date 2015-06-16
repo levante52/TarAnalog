6159e96daf04773625d50624e8ff05fda9b622b7
Valeriya Koryukina <koryukinavaleria@live.ru>
2015-06-16 09:34:50 +0300

    first commit

---
+++ TarAnalog/TarAnalog.cpp
@@ -0,0 +1,12 @@
+// TarAnalog.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// this line is for commit 1
+	return 0;
+}
+
