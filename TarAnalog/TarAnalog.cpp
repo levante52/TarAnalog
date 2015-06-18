55d2c7fac8063649463060d8ece7880b678e7a3d
Valeriya Koryukina <koryukinavaleria@live.ru>
2015-06-18 13:13:51 +0300

    Первые наработки

--- TarAnalog/TarAnalog.cpp
+++ TarAnalog/TarAnalog.cpp
@@ -2,11 +2,24 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <fstream>
 
-
+void filecopy(std::ifstream &in, std::ofstream &out)
+{
+	char ch;
+	while (in.get(ch))        
+		out.put(ch);
+}
 int _tmain(int argc, _TCHAR* argv[])
 {
-	// this line is for commit 1
+
+	std::ifstream first("D:\\1.bin", std::ios::binary);
+	std::ifstream second("D:\\2.bin", std::ios::binary);
+	std::ofstream outfile("D:\\3.bin", std::ios::app | std::ios::binary);
+	if (first) filecopy(first, outfile);
+	if (second) filecopy(second, outfile);
+
 	return 0;
 }
 
