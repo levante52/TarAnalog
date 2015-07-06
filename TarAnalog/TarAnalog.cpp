620dd06b7930c71a0e917816ee32e935a6ac0e90
Valeriya Koryukina <koryukinavaleria@live.ru>
2015-07-06 19:17:14 +0300

    Использование fread/fwrite в алгоритме слияния файлов

--- TarAnalog/TarAnalog.cpp
+++ TarAnalog/TarAnalog.cpp
@@ -4,21 +4,34 @@
 #include "stdafx.h"
 #include <iostream>
 #include <fstream>
+#include <cstdio>
+#include <string>
 
-void filecopy(std::ifstream &in, std::ofstream &out)
-{
-	char ch;
-	while (in.get(ch))        
-		out.put(ch);
-}
+
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 
-	std::ifstream first("D:\\1.bin", std::ios::binary);
-	std::ifstream second("D:\\2.bin", std::ios::binary);
-	std::ofstream outfile("D:\\3.bin", std::ios::app | std::ios::binary);
-	if (first) filecopy(first, outfile);
-	if (second) filecopy(second, outfile);
+	char byte[1];  // буфер для считывания одного байта
+
+	FILE *archFile; // файл - архив
+	FILE *file;   // файл для архивирования
+	errno_t err1, err2;
+
+	err1 = fopen_s(&archFile, "archFile.bin", "wb");
+	err2 = fopen_s(&file, "curFile.bin","rb");
+	// переписываем информацию в архив
+	if (err1 == 0 && err2 == 0)
+	{
+		while (!feof(file))
+		{
+			if (fread(byte, 1, 1, file) == 1)
+				fwrite(byte, 1, 1, archFile);
+		}
+	}
+
+	fclose(file);
+	fclose(archFile);
 
 	return 0;
 }
